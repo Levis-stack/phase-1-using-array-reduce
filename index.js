b76882ba72280ef64dfe43caa9f016d047ebeaf5
Levis <Levis.mwangi@student.moringaschool.com>
2025-03-14 14:50:20 +0300

    commit changes

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+function sumBatteries(total, batch) {
+    return total + batch;
+}
+
+const totalBatteries = batteryBatches.reduce(sumBatteries, 0);
+
+console.log(totalBatteries); // Output: 31
